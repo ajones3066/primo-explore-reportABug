4cdffd76ec4438940a48e52f31e0279a2ba74fd4
ajones3066 <jonesa@newschool.edu>
2018-06-17 08:26:23 -0400

    initial commit

---
+++ reportABug2.js
@@ -0,0 +1,22 @@
+app.controller('reportABugController', [function () {
+    
+     //set the context
+     var vm = this;
+    
+     //binds the function to the scope so it's requestable in the component.
+     vm.getRecordID = getRecordID;
+     vm.linkText = "Report A Problem 2";
+
+     //define the function that retrieves the record ID
+     function getRecordID() {
+         return vm.parentCtrl.item.pnx.control.recordid[0];
+     }
+
+ }]);
+
+app.component('prmServiceLinksAfter', {
+     bindings: {parentCtrl: '<'},
+     controller: 'reportABugController',
+     template: `<span class="md-subhead"><a href="http:\/\/www.google.com?q={{$ctrl.getRecordID()}}" target="_blank">{{ $ctrl.linkText }}</a></span>`
+
+ });
